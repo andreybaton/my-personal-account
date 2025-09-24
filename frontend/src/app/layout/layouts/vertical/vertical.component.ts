3d24cc5945918e8d7bfc763ba82926043a8e3d46
Андрей Колга <kolga.t@mail.ru>
2025-09-24 10:50:10 +0400

    Поправил панельку слева, исправил табуляцию

--- frontend/src/app/layout/layouts/vertical/vertical.component.ts
+++ frontend/src/app/layout/layouts/vertical/vertical.component.ts
@@ -1,36 +1,29 @@
 import { Component } from '@angular/core';
 
 interface MenuItem {
-  name: string;
-  description?: string;
-  route: string;
+    name: string;
+    description?: string;
+    route: string;
 }
 
 @Component({
-  selector: 'vertical-layout',
-  templateUrl: './vertical.component.html',
-//   styleUrls: ['./dashboard.component.scss']
+    selector: 'vertical-layout',
+    templateUrl: './vertical.component.html',
+
 })
 export class VerticalLayoutComponent {
-  dashboards: MenuItem[] = [
-    { name: 'Project', route: '/project' },
-    { name: 'Analytics', route: '/analytics' },
-    { name: 'Finance', route: '/finance' },
-    { name: 'Crypto', route: '/crypto' },
-    { name: 'Engines', route: '/engines' },
-    { name: 'EnginesAdmin', route: '/engines-admin' }
-  ];
+    dashboards: MenuItem[] = [
+        { name: 'Abiturientu', route: '/abiturientu' },
+        { name: 'Profile', route: '/profile' },
+        { name: 'Sistema soprovod', route: '/system' },
+        { name: 'Moodle', route: '/moodle' },
+        { name: 'Inf resources', route: '/inf' },
+        { name: 'Pers dock', route: '/pers' }
+    ];
 
-  applications: MenuItem[] = [
-    { name: 'Academy', route: '/academy' },
-    { name: 'Chat', route: '/chat' },
-    { name: 'Contacts', route: '/contacts' },
-    { name: 'ECommerce', route: '/ecommerce' }
-  ];
-
-  navigateTo(route: string): void {
-    // Навигация по роуту
-    console.log('Navigating to:', route);
-    // this.router.navigate([route]);
-  }
+    navigateTo(route: string): void {
+        // Навигация по роуту
+        console.log('Navigating to:', route);
+        // this.router.navigate([route]);
+    }
 }
